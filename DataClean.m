6d1b5fcb7285586e4b59b8b78ce216c1eafa99da
Emil Ekblom <ekblomemil@gmail.com>
2020-02-02 13:16:53 +0100

    Progress

--- DataClean.m
+++ DataClean.m
@@ -25,11 +25,11 @@ expDates_time    =  datetime(table2array(Prices(:, 2)), 'ConvertFrom', 'yyyyMMdd
 expDates         = yyyymmdd(expDates_time);
 expDatesNumeric  = datenum(expDates_time);
 
-OptionPrices = [Prices(:, 1:2), Prices(:, 4:7) Prices(:, 12:13)]; %Save data needed for return computations
+OptionPrices = [Prices(:, 1:2), Prices(:, 4:8) Prices(:, 12:13)]; %Save data needed for return computations
 
 %Clean Option Prices
-isCloseToMat = ((expDatesNumeric - datesNumeric) <= 8);   %Identify Options close to maturity
-OptionPrices = OptionPrices(isCloseToMat, :);             %Keep Option Prices of options that are close to maturity
+isCloseToMat = ((expDatesNumeric - datesNumeric) <= 8);    %Identify Options close to maturity
+OptionPrices = OptionPrices(isCloseToMat, :);              %Keep Option Prices of options that are close to maturity
 
 AMSettlement = (table2array(OptionPrices(:, end)) == 1);   %Create logical for AM-Settlement
 OptionPrices = OptionPrices(AMSettlement, :);              %Keep Option Prices of AM-settled options
@@ -70,17 +70,20 @@ end
 OTMFlag = OTMFlag(2:end); %Delete inital value
 isOTM   = (OTMFlag == 1);
 
-OTMOptionPrices = OptionPricesArray(isOTM, :); %Keep OTM Options
+OptionPricesArray = [OptionPricesArray, OTMFlag];
+
+%OTMOptionPrices = OptionPricesArray(isOTM, :); %Keep OTM Options
 
 
 %% Compute Option Returns
+
 %identify month changes for OTM Option Prices array
-DatesTrimmed = round(OTMOptionPrices(:, 1)./100);
+DatesTrimmed = round(OptionPricesArray(:, 1)./100);
 
-isFirstDay = zeros(size(OTMOptionPrices, 1), 1);
-isLastDay  = zeros(size(OTMOptionPrices, 1), 1);
+isFirstDay = zeros(size(OptionPricesArray, 1), 1);
+isLastDay  = zeros(size(OptionPricesArray, 1), 1);
 
-for i = 1:size(OTMOptionPrices, 1) - 1
+for i = 1:size(OptionPricesArray, 1) - 1
     if DatesTrimmed(i) < DatesTrimmed(i + 1)
         isFirstDay(i + 1) = 1;
         isLastDay(i)      = 1;
@@ -90,47 +93,89 @@ end
 isFirstDay(1) = 1;
 isLastDay(end) = 1;
 
-
 FirstDayList = find(isFirstDay);
 LastDayList  = find(isLastDay);
 
-OTMOptionDates = OTMOptionPrices(:,1);
-RolloverDates  = OTMOptionDates(FirstDayList);
+OptionDates = OptionPricesArray(:,1);
+RolloverDates  = OptionDates(FirstDayList);
 
 
 
 
 %% Sort Options on volume on every first trading day
 nAssets = 5;
+SortedOptions = ones(1, 10);
+nMonths = size(FirstDayList, 1);
+lag = 1;
 
-%Test without loop
+%Identify Options to trade
+%for i = 1:nMonths 
    day = RolloverDates(1);  %Grab rollover date
    
-   isFirstDay     = ismember(OTMOptionPrices(:,1), day);     %Identify days corresponding to grabbed date
-   relevantPrices = OTMOptionPrices .* isFirstDay;           %Set irrelevant prices to zero
-   relevantPrices(relevantPrices(:,1) == 0, :) = [];         %Delete irrelevant prices
+   isFirstDay     = ismember(OptionPricesArray(:,1), day);   %Identify days corresponding to grabbed date
+   firstDayOptions = OptionPricesArray .* isFirstDay;        %Set irrelevant prices to zero
+   firstDayOptions(firstDayOptions(:,1) == 0, :) = [];       %Delete irrelevant prices
    
-   volume = relevantPrices(:, 6);
-   FirstDayID     = relevantPrices(:, 7);
+   OTM    = firstDayOptions(:, end);                         %Grab OTM Flag
+   OTM    = (OTM == 1);                                      %Create Logical
+   firstDayOptions = firstDayOptions(OTM, :);                %Kill ITM Options from portfolio construction
    
-   high = maxk(volume, nAssets);
-   optionList = find(ismember(volume, high));
-   optionID = FirstDayID(optionList);
+   IV         = firstDayOptions(:, 7);                       %Grab Implied Volatility
+   volume     = firstDayOptions(:, 6);                       %Grab volume from first day
+   FirstDayID = firstDayOptions(:, 8);                       %Grab ID's from first day
    
-   start = FirstDayList(1);
-   stop  = LastDayList(1);
+   highVolume = maxk(volume, nAssets);                       %Identify 5 highest volume options
+   highIV     = maxk(IV, nAssets);                           %Identify 5 highest IV options
+   optionList = find(ismember(volume, highVolume));          %Get index of highest volume options
+   optionID   = FirstDayID(optionList);                      %Grab ID of highest volume options
    
-   OptionPrices = OTMOptionPrices(start:stop, :); %grab option prices for one month
-   ID = OptionPrices(:, 7);                       %Grab option IDs
+   %Get Options for each month based on ID 
+   start = FirstDayList(1);                                  %Grab first day of month index
+   stop  = LastDayList(1);                                   %Grab last day of month index
    
-   isRightOption  = ismember(ID, optionID);                   %Identify options based on ID
-   relevantPrices = OptionPrices .* isRightOption;            %Set irrelevant prices to zero
-   relevantPrices(relevantPrices(:,1) == 0, :) = [];          %delete irrelevant prices
+   Options = OptionPricesArray(start:stop, :);               %grab option prices for one month
+   ID = Options(:, 8);                                       %Grab option IDs for one month
    
+   isRightOption  = ismember(ID, optionID);                  %Identify options based on ID
+   relevantPrices = Options .* isRightOption;                %Set irrelevant prices to zero
+   relevantPrices(relevantPrices(:,1) == 0, :) = [];         %delete irrelevant prices
    
+   bids = relevantPrices(:, 4);
+   %bidPrices = zeros(4, 5);
+   fin = size(relevantPrices, 1);
+   vec1 = (1: 5 :fin);
+   vec2  = [vec1(2:end) - 1, fin];
+   bidmat = nan(1, 5);
    
+   for i = 1:4
+      start = vec1(i);
+      stop  = vec2(i);
+      
+      bidPrices = bids(start:stop)' ;
+      bidmat = [bidmat; bidPrices];
+      bidmat = [holdingDays, bidmat];
+   
+   end
 
    
+    %bidmat = bidmat(2:end,:);
+    %holdingDays = unique(relevantPrices(:,1));
+    %bidmat = [holdingDays, bidmat];
+
+    %nHoldingDays = size(holdingDays, 1);   
+   
+   
+   %SortedOptions = [SortedOptions; relevantPrices];
+%end
+
+%SortedOptions = SortedOptions(2:end, :);
+
+
+
+
+
+return
+BidPrices = zeros(nDays, nAssets);
    
    
    
