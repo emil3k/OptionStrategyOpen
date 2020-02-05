7f5d91ec8b5f9a443841156cc05898491d178db5
Emil Ekblom <ekblomemil@gmail.com>
2020-02-05 17:36:41 +0100

    Some progress

--- DataClean.m
+++ DataClean.m
@@ -12,12 +12,12 @@ SP500   = SP500(51:end, :);
 Settlement = readtable('SettlementData.xlsx');
 SettlementPrice = table2array(Settlement(1:255, 4));
 
+FFDaily = readtable('FFDaily.xlsx');
+FFDaily = FFDaily(19170:24515, :);
 
-
-
-
+%% Set parameters and fix Rf
+RfDaily = table2array(FFDaily(:, end)) ./100;
 contractSize = 100;
-
 
 %Extract data we need
 Delta  = Prices.DeltaOfTheOption; %Save greeks separately
@@ -109,21 +109,26 @@ isLastDay(end) = 1;
 FirstDayList = find(isFirstDay);
 LastDayList  = find(isLastDay);
 
-
 OptionDates = OptionPricesArray(:,1);
 RolloverDates  = OptionDates(FirstDayList);
+
+%Identify monthchanges for uniqe dates
+[uniqueFirstDayList, uniqueLastDayList] = getFirstAndLastDayInPeriod(datesUnique, 2);
+
+
 
 
 %% Sort Options on volume on every first trading day
 nShorts = 5;
-nAssets = 15;
+nAssets = 19;
 SortedOptions = ones(1, 10);
 nMonths = size(FirstDayList, 1);
 lag = 1;
+cash = 1;
 
 %Identify Options to trade
-%for i = 1:nMonths 
-   day = RolloverDates(1);  %Grab rollover date
+for i = 1:nMonths 
+   day = RolloverDates(i);  %Grab rollover date
    
    isFirstDay     = ismember(OptionPricesArray(:, 1), day);  %Identify days corresponding to rollover date
    firstDayOptions = OptionPricesArray .* isFirstDay;        %Set irrelevant prices to zero
@@ -142,6 +147,7 @@ lag = 1;
    FirstDayID = firstDayOptions(:, 8);                       %Grab ID's from first day
    bidPrices  = firstDayOptions(:, 4);                       %Grab cleaned Bid Prices from first day
    strikePrices = firstDayOptions(:, 3) ./1000;              %Grab strikes and divide by 1000 to match index
+   expDate    = firstDayOptions(1, 2);                       %Grab expiration date
    
    highVolume = maxk(volume, nAssets);                       %Identify 15 highest volume options
    optionListVolume = find(ismember(volume, highVolume));    %Get index of highest volume options
@@ -151,16 +157,40 @@ lag = 1;
    optionListIV = find(ismember(IV, highIV));                %Get index of highest volume options
    optionID   = FirstDayID(optionListIV);                    %Grab option ID of 5 highest IV of the 15 highest volume optio
    bids       = bidPrices(optionListIV);
+   netDebit   = sum(bids);
    strikes    = strikePrices(optionListIV);
       
    %Compute settlement payoff
-   settlePrice = ones(nShorts, 1) .* SettlementPrice(1);     %Grab settlement price and match dimension
+   settlePrice = ones(nShorts, 1) .* SettlementPrice(i);     %Grab settlement price and match dimension
    payoff      = max(0, strikes - settlePrice);              %Compute settlement payoff from sold options
+   PL          = sum(payoff);
+   
+   startOfMonth = uniqueFirstDayList(i);
+   endOfMonth = uniqueLastDayList(i);
+   
+   %Rf = RfDaily(startOfMonth:EndOfMonth);
+   MonthLength = endOfMonth - startOfMonth;
+   monthlyAccount = zeros(MonthLength, 1);
+   %cash2 = [1; cash];
+        for j = 1:MonthLength
+            if j == 1
+                monthlyAccount(j) = cash(end).*(1 + RfDaily(startOfMonth + j - 1));
+            
+            elseif datesUnique(j) == day
+                monthlyAccount(j) = monthlyAccount(j - 1) .* (1 + RfDaily(startOfMonth + j - 1)) + netDebit;
+            elseif datesUnique(j) == expDate
+                monthlyAccount(j) = monthlyAccount(j - 1) .* (1 + RfDaily(startOfMonth + j - 1)) - PL;        
+            else
+                monthlyAccount(j) = monthlyAccount(j - 1) .* (1 + RfDaily(startOfMonth + j - 1));
+            end
+         end
+   
+   cash = [cash; monthlyAccount];
    
    
-   
-   
-   
+end
+
+
    
    %Settlement = max(0, SettlePrice - Strike)
    
